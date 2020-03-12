88bd1c25bf1f393ddb93baba4467581d48048a0a
Muhammad Shoaib <muhammad.shoaib@tenpearls.com>
2020-03-12 18:34:45 +0500

    redux logger middleware added

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,15 +1,17 @@
 import { applyMiddleware, compose, createStore } from "redux";
 import createSagaMiddleware from "redux-saga";
 import { persistStore } from "redux-persist";
+import { createLogger } from 'redux-logger';
+
 
 import { rootReducer, rootSaga } from "./rootDuck";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+const loggerMiddleware = createLogger();
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(sagaMiddleware))
+    composeEnhancers(applyMiddleware(sagaMiddleware, loggerMiddleware))
 );
 
 /**

127bf2b49ec05375820e38bd22f1a7d65405970b
Muhammad Shoaib <muhammad.shoaib@tenpearls.com>
2020-03-16 15:40:56 +0500

    category cards design completed

--- src/app/pages/home/Dashboard.js
+++ src/app/pages/home/Dashboard.js
@@ -1,156 +1,103 @@
 import React, { useMemo } from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import {
   Portlet,
   PortletBody,
   PortletHeader,
   PortletHeaderToolbar
 } from "../../partials/content/Portlet";
-import { metronic } from "../../../_metronic";
-import QuickStatsChart from "../../widgets/QuickStatsChart";
-import OrderStatisticsChart from "../../widgets/OrderStatisticsChart";
-import OrdersWidget from "../../widgets/OrdersWidget";
-import SalesBarChart from "../../widgets/SalesBarChart";
-import DownloadFiles from "../../widgets/DownloadFiles";
-import NewUsers from "../../widgets/NewUsers";
-import LatestUpdates from "../../widgets/LatestUpdates";
-import BestSellers from "../../widgets/BestSellers";
-import RecentActivities from "../../widgets/RecentActivities";
-import PortletHeaderDropdown from "../../partials/content/CustomDropdowns/PortletHeaderDropdown";
+import OrderImage from "../../../_metronic/layout/assets/layout-svg-icons/order-2.svg";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
-  const { brandColor, dangerColor, successColor, primaryColor } = useSelector(
-    state => ({
-      brandColor: metronic.builder.selectors.getConfig(
-        state,
-        "colors.state.brand"
-      ),
-      dangerColor: metronic.builder.selectors.getConfig(
-        state,
-        "colors.state.danger"
-      ),
-      successColor: metronic.builder.selectors.getConfig(
-        state,
-        "colors.state.success"
-      ),
-      primaryColor: metronic.builder.selectors.getConfig(
-        state,
-        "colors.state.primary"
-      )
-    })
-  );
+  const categories = [
+    {
+      categoryTitle: 'Dry Cleaning',
+      categoryPrice: '$5.00',
+      categoryImage: 'https://image.flaticon.com/icons/svg/1818/1818882.svg',
+    },
+    {
+      categoryTitle: 'Wash & Fold',
+      categoryPrice: '$8.00',
+      categoryImage:
+        'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/among_nature_p1xb.svg',
+    },
+    {
+      categoryTitle: 'Commercial',
+      categoryPrice: '$2.00',
+      categoryImage: 'https://flatvectors.com/wp-content/uploads/travel-man.svg',
+    },
+    {
+      categoryTitle: 'Dry Cleaning',
+      categoryPrice: '$5.00',
+      categoryImage: 'https://image.flaticon.com/icons/svg/1818/1818882.svg',
+    },
+    {
+      categoryTitle: 'Wash & Fold',
+      categoryPrice: '$8.00',
+      categoryImage:
+        'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/among_nature_p1xb.svg',
+    },
+    // {
+    //   categoryTitle: 'Commercial',
+    //   categoryPrice: '$2.00',
+    //   categoryImage: 'https://flatvectors.com/wp-content/uploads/travel-man.svg',
+    // },
+  ];
+ 
 
-  const chartOptions = useMemo(
-    () => ({
-      chart1: {
-        data: [10, 14, 18, 11, 9, 12, 14, 17, 18, 14],
-        color: brandColor,
-        border: 3
-      },
-
-      chart2: {
-        data: [11, 12, 18, 13, 11, 12, 15, 13, 19, 15],
-        color: dangerColor,
-        border: 3
-      },
-
-      chart3: {
-        data: [12, 12, 18, 11, 15, 12, 13, 16, 11, 18],
-        color: successColor,
-        border: 3
-      },
-
-      chart4: {
-        data: [11, 9, 13, 18, 13, 15, 14, 13, 18, 15],
-        color: primaryColor,
-        border: 3
-      }
-    }),
-    [brandColor, dangerColor, primaryColor, successColor]
-  );
+  
 
   return (
     <>
       <div className="row">
         <div className="col-xl-6">
-          <div className="row row-full-height">
-            <div className="col-sm-12 col-md-12 col-lg-6">
-              <Portlet className="kt-portlet--height-fluid-half kt-portlet--border-bottom-brand">
-                <PortletBody fluid={true}>
-                  {/* <QuickStatsChart
-                    value={570}
-                    desc="Total Sales"
-                    data={chartOptions.chart1.data}
-                    color={chartOptions.chart1.color}
-                    border={chartOptions.chart1.border}
-                  /> */}
-                </PortletBody>
-              </Portlet>
+          <div className="row row-full-height ">
+            {categories.map((data, i) => {
+              return (
+                <div className="col-md-6 ">
+                  <Portlet className="justify-content-center category-card kt-portlet--border-bottom-brand">
+                    <PortletBody className="justify-content-center align-items-center" >
+                      <h5>{data.categoryTitle}</h5>
+                      <img className="category-image" alt="img" src={data.categoryImage} />
+                      <h6>Starting From</h6>
+                      <h2 className="font-weight-bold" >{data.categoryPrice}</h2>
+                    </PortletBody>
+                  </Portlet>
+                </div>
+              )
+            })
+            }
+            {/* <div className="kt-space-20" /> */}
 
-              <div className="kt-space-20" />
-
-              <Portlet className="kt-portlet--height-fluid-half kt-portlet--border-bottom-brand">
-                <PortletBody fluid={true}>
-                  <QuickStatsChart
-                    value={680}
-                    desc="Completed Transactions"
-                    data={chartOptions.chart2.data}
-                    color={chartOptions.chart2.color}
-                    border={chartOptions.chart2.border}
-                  />
-                </PortletBody>
-              </Portlet>
-            </div>
-
-            <div className="col-sm-12 col-md-12 col-lg-6">
-              <Portlet className="kt-portlet--height-fluid-half kt-portlet--border-bottom-brand">
-                <PortletBody fluid={true}>
-                  <QuickStatsChart
-                    value="234+"
-                    desc="Transactions"
-                    data={chartOptions.chart3.data}
-                    color={chartOptions.chart3.color}
-                    border={chartOptions.chart3.border}
-                  />
-                </PortletBody>
-              </Portlet>
-
-              <div className="kt-space-20" />
-
-              <Portlet className="kt-portlet--height-fluid-half kt-portlet--border-bottom-brand">
-                <PortletBody fluid={true}>
-                  <QuickStatsChart
-                    value="4.4M$"
-                    desc="Paid Commissions"
-                    data={chartOptions.chart4.data}
-                    color={chartOptions.chart4.color}
-                    border={chartOptions.chart4.border}
-                  />
-                </PortletBody>
-              </Portlet>
-            </div>
           </div>
         </div>
 
         <div className="col-xl-6">
           <Portlet fluidHeight={true}>
             <PortletHeader
+              icon={
+                <img className="mr-2" alt={"icon"} src={OrderImage} />
+              }
               title="Order Statistics"
               toolbar={
                 <PortletHeaderToolbar>
-                  <PortletHeaderDropdown />
+                  {/* <PortletHeaderDropdown /> */}
+                  <Link to="">
+                   View all
+                  </Link>
                 </PortletHeaderToolbar>
               }
             />
 
             <PortletBody>
-              <OrderStatisticsChart />
+
             </PortletBody>
           </Portlet>
         </div>
       </div>
 
-      <Portlet>
+      {/* <Portlet>
         <PortletBody fit={true}>
           <div className="row row-no-padding row-col-separator-xl">
             <div className="col-xl-4">
@@ -170,9 +117,9 @@ export default function Dashboard() {
             </div>
           </div>
         </PortletBody>
-      </Portlet>
+      </Portlet> */}
 
-      <div className="row">
+      {/* <div className="row">
         <div className="col-xl-4">
           <DownloadFiles />
         </div>
@@ -182,7 +129,7 @@ export default function Dashboard() {
         <div className="col-xl-4">
           <LatestUpdates />
         </div>
-      </div>
+      </div> */}
 
       {/* <div className="row">
         <div className="col-xl-8"></div>
@@ -191,14 +138,14 @@ export default function Dashboard() {
         </div>
       </div> */}
 
-      <div className="row">
+      {/* <div className="row">
         <div className="col-xl-8">
           <BestSellers />
         </div>
         <div className="col-xl-4">
           <RecentActivities />
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
